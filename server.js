fb9eb57d56eae1a722e5d40b1b127d299aa85954
Payton Banks <payton.banks@gmail.com>
2020-09-18 23:19:37 -0400

    Server activated, routes established.

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var path = require("path");
+
+var fs = require("fs");
+
+require("./routes/apiRoutes");
+require("./routes/htmlRoutes");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.listen(PORT, function(){
+    console.log("App listening on PORT: " + PORT);
+});
