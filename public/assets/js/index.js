c3f93750f5cc4a08cdf9172fb4ed9def507ce8e0
Payton Banks <payton.banks@gmail.com>
2020-09-21 08:34:30 -0400

    update styles, index and js

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -3,6 +3,7 @@ const $noteText = $(".note-textarea");
 const $saveNoteBtn = $(".save-note");
 const $newNoteBtn = $(".new-note");
 const $noteList = $(".list-container .list-group");
+app.use(express.static("public"));
 
 // activeNote is used to keep track of the note in the textarea
 let activeNote = {};
